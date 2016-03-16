54578e9e2d0f917b8e644f0f46ce02887c54ac67
Germy <jalee@cmu.edu>
2016-03-16 03:44:54 -0400

    basic site functionality

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,5 +1,5 @@
 class Attachment < ActiveRecord::Base
   belongs_to :user
   mount_uploader :file, FileUploader
-  validates_presence_of :name, :email, :password, :password_confirmation
+  validates_presence_of :file
 end
